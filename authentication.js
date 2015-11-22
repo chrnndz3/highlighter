634cd8f8ee0fa0e8d26717b03b3c4d491d328b5a
unFearing <chen240sx@gmail.com>
2015-11-21 20:33:36 -0600

    testing authentication

--- authentication.js
+++ authentication.js
@@ -1,8 +1,10 @@
-var ref = new Firebase("https://highlighter.firebaseio.com");
-ref.authWithOAuthPopup("facebook", function(error, authData) {
-  if (error) {
-    console.log("Login Failed!", error);
-  } else {
-    console.log("Authenticated successfully with payload:", authData);
-  }
-});
+function login() {
+	var ref = new Firebase("https://highlighter.firebaseio.com");
+	ref.authWithOAuthPopup("facebook", function(error, authData) {
+	  if (error) {
+	    console.log("Login Failed!", error);
+	  } else {
+	    console.log("Authenticated successfully with payload:", authData);
+	  }
+	});	
+}
