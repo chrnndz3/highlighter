912dff979f2337e0049a658be4f197d19d66ed63
unFearing <chen240sx@gmail.com>
2015-11-22 09:45:05 -0600

    asdasdsa

--- javascripts/chat.js
+++ javascripts/chat.js
@@ -31,10 +31,10 @@ messageField.keypress(function (e) {
     }
     console.log(curGroup);
     console.log(curGroup.key());
-    messagesRef.once('child_added', function(data) {
+    //messagesRef.once('child_added', function(data) {
       curMessage = messagesRef.child(curGroup.key()).push({id:id, name:username, text:message});
       messageField.val('');
-    });
+    //});
   }
 });
 
