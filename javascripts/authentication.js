6d3b11dc026b11526c185b11f6d1a7c35c8df740
unFearing <chen240sx@gmail.com>
2015-11-22 01:30:18 -0600

    sadsdssdasdas

--- javascripts/authentication.js
+++ javascripts/authentication.js
@@ -30,7 +30,7 @@ function login() {
 			users.once("value", function(snapshot) {
 				var len = snapshot.numChildren();
 				for (var i = 0; i < len; i++) {
-					console.log(users[i]);
+					console.log(snapshot[i]);
 				}
 			});
 			
