2de96e1895954be9c827eb11658cdde3ebeea69a
unFearing <chen240sx@gmail.com>
2015-11-21 21:15:16 -0600

    no fb login

--- javascripts/authentication.js
+++ javascripts/authentication.js
@@ -1,31 +1,6 @@
 function login() {
-	window.fbAsyncInit = function() {
-	    FB.init({
-	      appId      : '437792143082643',
-	      xfbml      : true,
-	      version    : 'v2.5'
-	    });
-		FB.getLoginStatus(function(response) {
-			if (response.status === 'connected') {
-			connectedonsole.log('Logged in.');
-			}
-			else {
-				FB.login();
-			}
-		});
-	  };
-
-
-	  (function(d, s, id){
-	     var js, fjs = d.getElementsByTagName(s)[0];
-	     if (d.getElementById(id)) {return;}
-	     js = d.createElement(s); js.id = id;
-	     js.src = "//connect.facebook.net/en_US/sdk.js";
-	     fjs.parentNode.insertBefore(js, fjs);
-	   }(document, 'script', 'facebook-jssdk'));
-
 	
-	/*
+	
 	var ref = new Firebase("https://highlighter.firebaseio.com");
 	ref.authWithOAuthPopup("facebook", function(error, authData) {
 		remember: "sessionOnly",
@@ -35,5 +10,5 @@ function login() {
 			console.log("Authenticated successfully with payload:", authData);
 		}
 	});	
-*/
+
 }
