c9f9c85ad48daa374a22bd88e1ecbf63a50fdd07
unFearing <chen240sx@gmail.com>
2015-11-21 21:23:22 -0600

    asdasdsdsa

--- javascripts/authentication.js
+++ javascripts/authentication.js
@@ -1,11 +1,12 @@
 function login() {
 	var ref = new Firebase("https://highlighter.firebaseio.com");
 	ref.authWithOAuthPopup("facebook", function(error, authData) {
-		remember: "sessionOnly",
 		if (error) {
 			console.log("Login Failed!", error);
 		} else {
 			console.log("Authenticated successfully with payload:", authData);
 		}
+	}, {
+		remember: "sessionOnly",
 	});	
 }
