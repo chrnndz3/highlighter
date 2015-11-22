9741d797ea055bd6140fc9327393d17e6b0670c3
unFearing <chen240sx@gmail.com>
2015-11-21 20:54:30 -0600

    test again

--- javascripts/authentication.js
+++ javascripts/authentication.js
@@ -1,4 +1,19 @@
 function login() {
+	window.fbAsyncInit = function() {
+	    FB.init({
+	      appId      : '437792143082643',
+	      xfbml      : true,
+	      version    : 'v2.5'
+	    });
+	  };
+
+	  (function(d, s, id){
+	     var js, fjs = d.getElementsByTagName(s)[0];
+	     if (d.getElementById(id)) {return;}
+	     js = d.createElement(s); js.id = id;
+	     js.src = "//connect.facebook.net/en_US/sdk.js";
+	     fjs.parentNode.insertBefore(js, fjs);
+	   }(document, 'script', 'facebook-jssdk'));
 
 	FB.getLoginStatus(function(response) {
 	  if (response.status === 'connected') {
@@ -7,7 +22,8 @@ function login() {
 	  else {
 	    FB.login();
 	  }
-	});/*
+	});
+	/*
 	var ref = new Firebase("https://highlighter.firebaseio.com");
 	ref.authWithOAuthPopup("facebook", function(error, authData) {
 		remember: "sessionOnly",
